5d2d02a1cbabef22eab40f0de2bfad6c4458016c
Julien Durand <Juliend1305@gmail.com>
2025-11-19 15:39:13 +0100

    Add splineManager

---
+++ Assets/__Workspaces/Julien/Scripts/Managers/SplineManager.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.Splines;
+using Random = UnityEngine.Random;
+
+public class SplineManager : MonoBehaviour
+{
+    public SplineContainer SplineContainer;
+
+    public int KnotCount;
+    public int TerrainSize;
+    
+    // position du dernier point posé, utiliser pour générer loe départ du prochain point
+    private Vector3 _lastKnotPosition = Vector3.zero;
+        
+    private void OnEnable()
+    {
+        SplineContainer = GetComponent<SplineContainer>();
+    }
+
+    [ContextMenu("GenerateSpline")]
+    private void GenerateSpline()
+    {
+        SplineContainer.Spline.Clear(); // je clear la spline
+
+        float space = TerrainSize / (KnotCount - 1);
+        
+        for (float i = 0; i <= TerrainSize; i += space)
+        {
+            Vector3 posA = new Vector3(); // position du point A
+            Vector3 posB = new Vector3(); // Position du point B
+
+            if (i == 0) // Si le knot est le premier, alors on joue l'interieur du code
+            {
+                posA = new Vector3(Random.Range(4, 8), 0, i); // La position X du point A serra = à un random, et il n'y aurra pas de point B vu que c'est le premier
+            }
+            else // sinon
+            {
+                posA = new Vector3(_lastKnotPosition.x, 0, i); // La position sur X est celle du dernier point, et i est = à la longueur
+                posB = new Vector3(_lastKnotPosition.x + Random.Range(-4, 5), 0, i); // Le second point, le X est randomiser ( la valeur pourrait être tweeker en fonction de la difficulté )
+            }
+
+            
+            BezierKnot knotA = new BezierKnot(posA, Vector3.zero, Vector3.zero); //Je créer un Knot qui serra le point A
+            SplineContainer.Spline.Add(knotA, TangentMode.Linear); // je l'ajoute à ma spline, et le Knot serra Linear
+            
+            if (i > 0) // Si le knot n'est pas le premier alors on joue le code
+            {
+                BezierKnot knotB = new BezierKnot(posB, Vector3.zero, Vector3.zero);
+                SplineContainer.Spline.Add(knotB, TangentMode.Linear);
+            }
+
+            if (i == 0) // Si c'est le premier knot
+            {
+                _lastKnotPosition = posA; // lastKnotPosition serra égal au premier
+            }
+            else
+            {
+                _lastKnotPosition = posB; // lastKnotPosition serra égal au dernier porsé
+            }
+        }
+        
+        
+        
+        // Parcours spline
+        for (float i = 0; i <= 1; i += 0.01f)
+        {
+            SplineContainer.Evaluate(0, i, out float3 pos, out float3 tangent, out _);
+            // Sur la spline[0], je prend la pos i, la pos est normalizé, elle me renvoie la pos dans le monde.
+            float3 lastPos = pos;
+            Debug.Log(lastPos);
+        }
+    }
+}
